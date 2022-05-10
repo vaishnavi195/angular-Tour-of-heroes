8b6e02194ca3d6a8a469eae74cab56973b09d638
vaishnavi195 <varshitha.345@gmail.com>
2022-05-10 12:16:51 +0530

    2 files modified, 4 added

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css'],
+})
+export class HeroComponent implements OnInit {
+  hero: Hero = {
+    id: 1,
+    name: 'Iron Man',
+  };
+  constructor() {}
+
+  ngOnInit() {}
+}
